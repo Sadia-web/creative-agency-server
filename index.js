93cfc30704d17d54ee179e45c1ed250d1b3c2c7c
Sadia-web <sadiamim354@gmail.com>
2020-10-15 10:36:34 +0600

    Api working

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 
 const MongoClient = require('mongodb').MongoClient;
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bmdyz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 
 const port = 5000
 
@@ -22,7 +24,7 @@ app.get('/', (req, res) => {
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bmdyz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const orderCollection = client.db("creativeAgencyTeam").collection("orders");
@@ -38,4 +40,41 @@ client.connect(err => {
         })
 })
 
+// insert review info to database
+app.post('/addReview', (req, res) => {
+    const review = req.body;
+    console.log(review);
+    reviewCollection.insertOne(review)
+        .then((result) => {
+            res.send(result.insertedCount > 0)
+        })
+})
+
+app.get('/reviews', (req, res) => {
+    reviewCollection.find({}).sort({ _id: -1 }).limit(3)
+        .toArray((err, documents) => {
+            res.send(documents);
+        })
+})
+
+app.post('/addService', (req, res) => {
+    const service = req.body;
+    console.log(service);
+    servicesCollection.insertOne(service)
+        .then((result) => {
+            res.send(result.insertedCount > 0)
+        })
+
+})
+
+app.get('/getServices', (req, res) => {
+    servicesCollection.find({})
+    .toArray((err, documents) => {
+        res.send(documents)
+    })
+})
+
+
 });
+
+app.listen(process.env.PORT || port)
